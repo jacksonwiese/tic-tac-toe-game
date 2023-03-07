5f1e86bae666fd07db3150f112691f24d7ca019e
jacksonwiese <jacksonwiese1@gmail.com>
2023-03-07 17:53:49 -0600

    Initial app creation, no coding yet!

---
+++ tac-tac-toe-game/tac-tac-toe-game/tac_tac_toe_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  tac_tac_toe_gameApp.swift
+//  tac-tac-toe-game
+//
+//  Created by Jackson Wiese on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct tac_tac_toe_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
